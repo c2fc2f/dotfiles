45e3d013c900ed2ce52adc4a32c7428268448f5d
c2fc2f <culottes@sagbot.com>
2025-12-15 20:43:29 +0100

    chore(feishin): update to lastest version

--- modules/workstation/programs/players/feishin/default.nix
+++ modules/workstation/programs/players/feishin/default.nix
@@ -4,11 +4,11 @@
   home-manager.users.${username} = {
     home.packages = [
       (pkgs.feishin.overrideAttrs (old: rec {
-        version = "1.0.0-beta.10";
+        version = "1.0.0-beta.11";
 
         src = pkgs.fetchzip {
           url = "https://github.com/jeffvli/feishin/archive/refs/tags/v${version}.tar.gz";
-          sha256 = "sha256-6NCOHeEctiKQGlajJOSoGOOApoUM3ZXt/HyWzdNgqBk=";
+          sha256 = "sha256-+cXQJ0qb0n08gn53vPqEpHg9t6mnOZgsE/H+LTc0SW4=";
         };
 
         pnpmDeps = pkgs.pnpm.fetchDeps {
