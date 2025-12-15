fa75c21e6255f137ba727e58d6998ea9250aa00e
c2fc2f <contact@c2fc2f.com>
2025-12-15 07:40:49 +0100

    feat(ssh): add sshfs

---
+++ modules/core/programs/ssh/ssh.nix
@@ -0,0 +1,34 @@
+{ username, ... }:
+/*
+  SSH client configuration for the user, allowing customization of SSH
+  settings such as known hosts, identities, and connection preferences.
+  Useful for managing secure remote access to servers.
+*/
+{
+  home-manager.users.${username} = {
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        "*" = {
+          addKeysToAgent = "yes";
+        };
+        "github.com" = {
+          user = username;
+          identityFile = "~/.ssh/${username}";
+          identitiesOnly = true;
+        };
+        "sisyphe.sagbot.com" = {
+          user = username;
+          identityFile = "~/.ssh/${username}";
+          identitiesOnly = true;
+        };
+        "icare.sagbot.com" = {
+          user = username;
+          identityFile = "~/.ssh/${username}";
+          identitiesOnly = true;
+        };
+      };
+    };
+  };
+}
