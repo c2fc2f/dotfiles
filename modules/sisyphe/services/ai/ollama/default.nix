da15660b5c5c95a72a43bde260e7a94c800657f3
c2fc2f <contact@c2fc2f.com>
2025-12-15 07:36:59 +0100

    feat(ollama): add more qwen models and remove deepseak model

--- modules/sisyphe/services/ai/ollama/default.nix
+++ modules/sisyphe/services/ai/ollama/default.nix
@@ -10,10 +10,12 @@
     };
 
     loadModels = [
+      "qwen3:0.6b"
+      "qwen3:1.7b"
+      "qwen3:4b"
       "qwen3:8b"
-      "mistral:7b"
+      "qwen3:14b"
       "embeddinggemma:latest"
-      "deepseek-r1:32b"
     ];
   };
 }
