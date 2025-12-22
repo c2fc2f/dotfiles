3a7ba954dcb7d95b4ccd6846af891b071444cffb
c2fc2f <contact@c2fc2f.com>
2025-12-22 19:59:59 +0100

    feat(ci/update): add to curl --retry

--- modules/sisyphe/services/databases/neo4j/plugins/apoc/ci/update.sh
+++ modules/sisyphe/services/databases/neo4j/plugins/apoc/ci/update.sh
@@ -9,7 +9,7 @@ PACKAGE_PATH=$(readlink -f "${SCRIPT_DIR}/../default.nix")
 REPO_OWNER="neo4j"
 REPO_NAME="apoc"
 
-VERSION=$(curl -sS "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/latest" | jq -r ".tag_name" | sed s/v//)
+VERSION=$(curl -sS --retry 3 --retry-delay 2 --retry-all-errors "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/latest" | jq -r ".tag_name" | sed s/v//)
 
 FILE_NAME="apoc-${VERSION}-core.jar"
 
--- modules/sisyphe/services/databases/neo4j/plugins/neosemantics/ci/update.sh
+++ modules/sisyphe/services/databases/neo4j/plugins/neosemantics/ci/update.sh
@@ -9,7 +9,7 @@ PACKAGE_PATH=$(readlink -f "${SCRIPT_DIR}/../default.nix")
 REPO_OWNER="neo4j-labs"
 REPO_NAME="neosemantics"
 
-VERSION=$(curl -sS "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/latest" | jq -r ".tag_name" | sed s/v//)
+VERSION=$(curl -sS --retry 3 --retry-delay 2 --retry-all-errors "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/latest" | jq -r ".tag_name" | sed s/v//)
 
 FILE_NAME="neosemantics-${VERSION}.jar"
 
