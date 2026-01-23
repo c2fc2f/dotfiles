961ac8059e2b4c2d689ca3ad163a96cb1bbf4dbb
c2fc2f <contact@c2fc2f.com>
2026-01-23 10:51:18 +0100

    feat(ssh): auto generate ssh/cpnfig for all servers

--- modules/core/programs/ssh/ssh.nix
+++ modules/core/programs/ssh/ssh.nix
@@ -1,9 +1,19 @@
-{ username, ... }:
+{
+  lib,
+  username,
+  systemInfo,
+  ...
+}:
 /*
   SSH client configuration for the user, allowing customization of SSH
   settings such as known hosts, identities, and connection preferences.
   Useful for managing secure remote access to servers.
 */
+let
+  serverHosts = lib.filter (
+    name: lib.elem "server" systemInfo.${name}.groups && !(lib.elem "iso" systemInfo.${name}.groups)
+  ) (lib.attrNames systemInfo);
+in
 {
   home-manager.users.${username} = {
     programs.ssh = {
@@ -18,17 +28,18 @@
           identityFile = "~/.ssh/${username}";
           identitiesOnly = true;
         };
-        "sisyphe.sagbot.com" = {
-          user = username;
-          identityFile = "~/.ssh/${username}";
-          identitiesOnly = true;
-        };
-        "icare.sagbot.com" = {
-          user = username;
-          identityFile = "~/.ssh/${username}";
-          identitiesOnly = true;
-        };
-      };
+      }
+      // (builtins.listToAttrs (
+        map (name: {
+          inherit name;
+          value = {
+            hostname = "${name}.sagbot.com";
+            user = username;
+            identityFile = "~/.ssh/${username}";
+            identitiesOnly = true;
+          };
+        }) serverHosts
+      ));
     };
   };
 }
