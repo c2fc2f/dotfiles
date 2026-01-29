b8049e45b87e4a21791dc9ae4de7bb72ef762ac9
c2fc2f <contact@c2fc2f.com>
2026-01-29 13:49:39 +0100

    feat(core/nvf): handle clipboard if connected with SSH

--- modules/core/programs/vim/nvf/clipboard/default.nix
+++ modules/core/programs/vim/nvf/clipboard/default.nix
@@ -1,15 +1,33 @@
 { username, ... }:
 
 {
-  home-manager.users.${username}.programs.nvf.settings.vim = {
-    clipboard = {
-      enable = true;
-
-      providers.wl-copy = {
+  home-manager.users.${username}.programs = {
+    nvf.settings.vim = {
+      clipboard = {
         enable = true;
+
+        providers.wl-copy = {
+          enable = true;
+        };
+
+        registers = "unnamedplus";
       };
 
-      registers = "unnamedplus";
+      luaConfigRC.osc52 = ''
+        if vim.env.SSH_TTY ~= nil then
+          vim.g.clipboard = {
+            name = 'osc52',
+            copy = {
+              ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+              ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+            },
+            paste = {
+              ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+              ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+            },
+          }
+        end
+      '';
     };
   };
 }
