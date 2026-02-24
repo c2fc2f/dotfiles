346b81fed0faedb0b72beec51c6636533e2a3f77
c2fc2f <culottes@sagbot.com>
2026-02-24 16:05:27 +0100

    chore(workstation/spotify/spotx): update to lastest version

--- modules/workstation/programs/spotify/_spotx/default.nix
+++ modules/workstation/programs/spotify/_spotx/default.nix
@@ -2,7 +2,7 @@ final: prev:
 let
   spotx = prev.fetchurl {
     url = "https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/refs/heads/main/spotx.sh";
-    hash = "sha256-ymOCFOsLYdD2xfGuit/wyphav8sAYIJc7Gpo0PsWTrU=";
+    hash = "sha256-mNip8U6lOG0hLa2gF5i2ydqdsU5mGa0wMGc5Fhnd2w0=";
   };
 in
 {
