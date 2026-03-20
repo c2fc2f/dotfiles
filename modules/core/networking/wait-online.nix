842dfc2179af84c0277305011d997d59e2c02e2a
c2fc2f <contact@c2fc2f.com>
2026-03-20 14:49:33 +0100

    feat(core/networking): disable NetworkManager-wait-online.service

--- modules/core/networking/wait-online.nix
+++ modules/core/networking/wait-online.nix
@@ -1,4 +1,7 @@
 {
-  systemd.network.wait-online.enable = false;
+  systemd = {
+    network.wait-online.enable = false;
+    services.NetworkManager-wait-online.enable = false;
+  };
   boot.initrd.systemd.network.wait-online.enable = false;
 }
