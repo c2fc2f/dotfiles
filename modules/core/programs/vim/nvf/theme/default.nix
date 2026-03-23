45e229c8b9acf84cd787fdc0dfe8ee5db3247556
c2fc2f <contact@c2fc2f.com>
2026-03-23 12:46:19 +0100

    chore(theme): revert to white theme

--- modules/core/programs/vim/nvf/theme/default.nix
+++ modules/core/programs/vim/nvf/theme/default.nix
@@ -7,8 +7,8 @@
   home-manager.users.${username}.programs.nvf.settings.vim = {
     theme = {
       enable = true;
-      name = "github";
-      style = "dark_high_contrast";
+      name = "catppuccin";
+      style = "auto";
     };
   };
 }
