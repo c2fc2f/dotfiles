530a014dcd3897d307c4c878625312ecf7ba9349
c2fc2f <contact@c2fc2f.com>
2026-06-03 11:38:06 +0200

    feat(development/neovim/c): uncrustify as the formatter & add extra flags to clang

--- modules/development/programs/vim/nvf/languages/c/default.nix
+++ modules/development/programs/vim/nvf/languages/c/default.nix
@@ -6,39 +6,76 @@
 }:
 
 {
-  home-manager.users.${username}.programs.nvf.settings.vim = {
-    languages.clang = {
-      enable = true;
+  home-manager.users.${username} = {
+    programs.nvf.settings.vim = {
+      languages.clang = {
+        enable = true;
+        cHeader = true;
+      };
 
-      cHeader = true;
-    };
-
-    lsp.servers.clangd.cmd = lib.mkForce [
-      "${pkgs.clang-tools}/bin/clangd"
-      "--fallback-style=none"
-    ];
-
-    formatter.conform-nvim = {
-      enable = true;
-      setupOpts = {
-        formatters_by_ft = {
-          c = [ "uncrustify" ];
-          cpp = [ "uncrustify" ];
-        };
-        formatters = {
-          uncrustify = {
-            command = lib.getExe pkgs.uncrustify;
-            args = [
-              "-c"
-              "${./uncrustify078_c.cfg}"
-              "-l"
-              "C"
-              "-q"
-              "--no-backup"
-            ];
+      formatter.conform-nvim = {
+        enable = true;
+        setupOpts = {
+          formatters_by_ft = {
+            c = lib.mkOverride 5 [ "uncrustify" ];
+          };
+          formatters = {
+            uncrustify = {
+              command = lib.getExe pkgs.uncrustify;
+              args = [
+                "-c"
+                "${./uncrustify078_c.cfg}"
+                "-l"
+                "C"
+                "-q"
+                "--no-backup"
+              ];
+            };
           };
         };
       };
+
+      keymaps = [
+        {
+          key = "<F8>";
+          mode = "n";
+          action = "<cmd>!gcc -Wall -Wconversion -Werror -Wextra -Wpedantic -Wwrite-strings -std=c2x %<CR>";
+          desc = "Compile using gcc";
+        }
+        {
+          key = "<F9>";
+          mode = "n";
+          action = "<cmd>make<CR>";
+          desc = "Compile using make";
+        }
+        {
+          key = "<F10>";
+          mode = "n";
+          action = "<cmd>copen<CR>";
+          desc = "Open error panel";
+        }
+      ];
+
+      luaConfigRC.geany_make_setup = lib.mkAfter ''
+        vim.opt.makeprg = "make"
+
+        vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+          pattern = "[^l]*",
+          command = "cwindow"
+        })
+      '';
     };
+
+    home.file.".config/clangd/config.yaml".text = ''
+      CompileFlags:
+        Add: 
+          - "-std=c2y"
+          - "-Wall"
+          - "-Wconversion"
+          - "-Werror"
+          - "-Wextra"
+          - "-Wpedantic"
+          - "-Wwrite-strings"
+    '';
   };
 }
