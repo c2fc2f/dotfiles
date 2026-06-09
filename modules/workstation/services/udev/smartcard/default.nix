a98feb9cc8f53e36c4deac8ccf1373693a4d5d43
c2fc2f <contact@c2fc2f.com>
2026-06-09 09:17:22 +0200

    feat(workstation/udev): add custom udev rule for token2 key

--- modules/workstation/services/udev/smartcard/default.nix
+++ modules/workstation/services/udev/smartcard/default.nix
@@ -27,5 +27,13 @@ in
       ENV{ID_VENDOR_ID}=="349e",\
       ENV{ID_VENDOR}=="TOKEN2",\
       RUN+="${lib.getExe lockScript}"
+
+    ACTION=="add",\
+      ENV{ID_BUS}=="usb",\
+      ENV{ID_MODEL_ID}=="0024",\
+      ENV{ID_VENDOR_ID}=="349e",\
+      ENV{ID_VENDOR}=="TOKEN2",\
+      ATTR{bInterfaceClass}=="0b",\
+      RUN+="${pkgs.procps}/bin/pkill -TERM -f scdaemon"
   '';
 }
