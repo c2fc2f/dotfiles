48966717b2e2fc740fe50e0043a53ecc18ac8523
c2fc2f <culottes@sagbot.com>
2026-07-01 15:55:35 +0200

    chore(neo4j/plugins/apoc): update to v5.26.28

--- modules/sisyphus/services/databases/neo4j/_plugins/apoc/default.nix
+++ modules/sisyphus/services/databases/neo4j/_plugins/apoc/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "apoc";
-  version = "5.26.27";
+  version = "5.26.28";
 
   src = fetchurl {
     url = "https://github.com/neo4j/apoc/releases/download/${version}/apoc-${version}-core.jar";
-    hash = "sha256-WbsqQjds08jFTM3YBOuXS8cUOWVqujKPpyTk9lp9FJI=";
+    hash = "sha256-OZgm+orHWcX5QSG5HwDBPIauz/YoFy0zbE6BccX0me8=";
   };
 
   dontUnpack = true;
