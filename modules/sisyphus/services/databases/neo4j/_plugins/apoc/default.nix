2228467724486835c9a6d90c912b243e8100bc23
c2fc2f <culottes@sagbot.com>
2026-09-10 16:05:42 +0200

    chore(neo4j/plugins/apoc): update to v2026.08.1

--- modules/sisyphus/services/databases/neo4j/_plugins/apoc/default.nix
+++ modules/sisyphus/services/databases/neo4j/_plugins/apoc/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "apoc";
-  version = "2026.08.0";
+  version = "2026.08.1";
 
   src = fetchurl {
     url = "https://github.com/neo4j/apoc/releases/download/${version}/apoc-${version}-core.jar";
-    hash = "sha256-It6xZO8LWbxTbcecjdeUMWZ5VnkB6gCLditbg7SFiPg=";
+    hash = "sha256-hgfIc9+GeqGzuMmSbVUmLQkIjSzM1itTo19DzcIvhDM=";
   };
 
   dontUnpack = true;
