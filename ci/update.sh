c86605c0009848668cee7c845842ab45d08653e3
c2fc2f <contact@c2fc2f.com>
2026-09-18 09:16:00 +0200

    chore(*.sh): expend set -euo pipefail

--- ci/update.sh
+++ ci/update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
 
 SCRIPT_PATH=$(readlink -f "${BASH_SOURCE[0]}")
 SCRIPT_DIR=$(dirname -- "${SCRIPT_PATH}")
